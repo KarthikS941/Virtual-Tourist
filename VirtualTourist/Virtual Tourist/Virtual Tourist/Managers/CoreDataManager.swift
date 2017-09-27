338ebde68e9a8abd8f723a61fa1e77b724f2637a
Karthik Sankar <karthiks94one@gmail.com>
2017-09-26 22:02:45 -0500

    Initial commit

---
+++ VirtualTourist/Virtual Tourist/Virtual Tourist/Managers/CoreDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  MapManager.swift
+//  Virtual Tourist
+//
+//  Created by Karthik Sankar on 9/26/17.
+//  Copyright © 2017 Karthik Sankar. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataManager {
+    
+}
---
+++ VirtualTourist/Virtual Tourist/Virtual Tourist/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Karthik Sankar on 9/26/17.
+//  Copyright © 2017 Karthik Sankar. All rights reserved.
+//
+
+import UIKit
+import Mapbox // Import Map Box
+
+class MapViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
